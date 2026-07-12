ef30410d9c780c83940502fa719dfa1725be97c1
vath414 <dydyloy69@gmail.com>
2026-07-12 12:26:44 +0700

    add more

---
+++ main15.py
@@ -0,0 +1,9 @@
+from common import  setup,work,smooth_scroll
+import random, time
+
+if __name__ == "__main__":
+  # Only setup driver if selected
+    driver = setup("AdriHalim763")
+    # smooth_scroll(driver)
+    work(driver)
+#https://x.com/AvaAdam136653
